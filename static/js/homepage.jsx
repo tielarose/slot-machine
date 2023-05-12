98dcd827db4c653677f170f344bb16fabd64a494
Tiela Black-Law <tiela.rose@gmail.com>
2023-05-12 12:49:38 -0700

    Create App component

---
+++ static/js/homepage.jsx
@@ -0,0 +1,9 @@
+function App(props) {
+  return (
+    <React.Fragment>
+      <h1>this is from the App component</h1>
+    </React.Fragment>
+  );
+}
+
+ReactDOM.render(<App />, document.querySelector("#app"));
