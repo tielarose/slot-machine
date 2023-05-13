d8f9518b01c487721d2b964251f12db3a97df7c9
Tiela Black-Law <tiela.rose@gmail.com>
2023-05-12 17:48:54 -0700

    Add emojis, simplify message logic

--- static/js/homepage.jsx
+++ static/js/homepage.jsx
@@ -1,18 +1,13 @@
 function SlotMachine(props) {
   const { pic1, pic2, pic3 } = props;
-  let msg;
+  const isWinner = pic1 === pic2 && pic2 === pic3;
 
-  if (new Set([pic1, pic2, pic3]).size === 1) {
-    msg = "You win!";
-  } else {
-    msg = "You lose!";
-  }
   return (
     <React.Fragment>
       <p>
         {pic1} {pic2} {pic3}
       </p>
-      <p>{msg}</p>
+      <p>{isWinner ? "You win!" : "You lose!"}</p>
     </React.Fragment>
   );
 }
@@ -21,9 +16,9 @@ function App(props) {
   return (
     <React.Fragment>
       <h1>Slot Machines!</h1>
-      <SlotMachine pic1="X" pic2="Y" pic3="X" />
-      <SlotMachine pic1="Z" pic2="Z" pic3="Z" />
-      <SlotMachine pic1="Y" pic2="X" pic3="Z" />
+      <SlotMachine pic1="🍒" pic2="🍒" pic3="🍊" />
+      <SlotMachine pic1="🔔" pic2="🔔" pic3="🔔" />
+      <SlotMachine pic1="🍒" pic2="🍊" pic3="🔔" />
     </React.Fragment>
   );
 }
